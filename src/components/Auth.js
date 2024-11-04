efdff849d2f344cd11f902f00f4532e47f2b019a
kpilszak <klaudia.pilszak@gmail.com>
2024-11-04 04:31:37 -0500

    Style Auth component

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,57 @@
+import { useState } from 'react'
+
 const Auth = () => {
+    const [isLogin, setIsLogin] = useState(true)
+    const [username, setUsername] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
+    const [error, setError] = useState(false)
+
+    const handleSubmit = () => {
+        if (password !== confirmPassword) {
+            setError(true)
+            return
+        }
+    }
+
     return (
-        <div>Auth</div>
+        <div className="auth-container">
+            <div className="auth-container-box">
+                <div className="auth-container-form">
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="username"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        id="password"
+                        name="password"
+                        placeholder="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {!isLogin && <input
+                        type="text"
+                        id="password-check"
+                        name="password-check"
+                        placeholder="confirm password"
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />}
+                    {error && <p>*Make sure passwords match</p>}
+                    <button className='standard-button' onClick={handleSubmit}>GO!</button>
+                </div>
+                <div className="auth-options">
+                    <button onClick={() => setIsLogin(false)}
+                        style={{ backgroundColor: !isLogin ? '#151a1f' : '#070a0d'}}
+                        >Sign Up</button>
+                    <button onClick={() => setIsLogin(true)}
+                        style={{ backgroundColor: isLogin ? '#151a1f' : '#070a0d' }}
+                        >Login</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
