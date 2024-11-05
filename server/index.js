c387af29559cedfd67721e00eaa463f199e29944
kpilszak <klaudia.pilszak@gmail.com>
2024-11-05 05:17:40 -0500

    Create signup endpoint

--- server/index.js
+++ server/index.js
@@ -2,4 +2,14 @@ const PORT = 8000
 const express = require('express')
 const app = express()
 
+app.post('/signup', async (req, res) => {
+    try {
+        const { username, password } = req.body
+        console.log(username, password)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: error})
+    }
+})
+
 app.listen(PORT, () => console.log('Server running on PORT ' + PORT))
