97eb99abc032c6f67a888971b1b93ca8a0bacf25
kpilszak <klaudia.pilszak@gmail.com>
2024-11-11 05:40:00 -0500

    Add credentials to authenticate with getstream

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const PORT = 8000
 const express = require('express')
+const bcrypt = require('bcrypt')
+const { v1: uuidv1 } = require('uuid')
+const { connect } = require('getstream')
 const app = express()
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
+
+
 app.post('/signup', async (req, res) => {
     try {
         const { username, password } = req.body
+        
+        const userId = uuidv1()
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const client = connect(API_KEY, API_SECRET, APP_ID)
+        const token = client.createUserToken(userId)
+
+        res.status(200).json({ username, password, userId, hashedPassword, token })
+
         console.log(username, password)
     } catch (error) {
         console.log(error)
