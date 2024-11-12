988a7bcc1fb3f4c191b68648cd1963e0ca8e5c8c
kpilszak <klaudia.pilszak@gmail.com>
2024-11-12 05:56:08 -0500

    Use cookies to authenticate

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
 import React, { useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie'
 import { StreamChat } from 'stream-chat'
 import { Chat, Channel } from 'stream-chat-react'
 import Auth from './components/Auth'
 import MessagingContainer from './components/MessagingContainer'
 import Video from './components/Video'
 
-const filters = { type: 'messaging' }
-const options = { state: true, presence: true, limit: 10 }
-const sort = { last_message_at: -1 }
-
 const client = StreamChat.getInstance('6t3abd8grfbz')
 
 const App = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(['user'])
   const [clientReady, setClientReady] = useState(false);
   const [channel, setChannel] = useState(null);
 
-  const authToken = true
+  const authToken = cookies.AuthToken
 
-  useEffect(() => {
-    const setupClient = async () => {
-      try {
-        await client.connectUser(
-          {
-            id: 'dave-matthews',
-            name: 'Dave Matthews'
-          },
-          ''
-        )
+  const setupClient = async () => {
+    try {
+      await client.connectUser(
+        {
+          id: cookies.UserId,
+          name: cookies.Name,
+          hashedPassword: cookies.HashedPassword
+        },
+        authToken
+      )
 
-        const channel = await client.channel('gaming', 'gaming-demo', {
-          name: 'Gaming Demo'
-        })
-        setChannel(channel)
+      const channel = await client.channel('gaming', 'gaming-demo', {
+        name: 'Gaming Demo'
+      })
+      setChannel(channel)
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-        setClientReady(true);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    setupClient()
-  }, [])
-
-  if (!clientReady) return null;
+  if (authToken) setupClient()
 
   return (
     <>
