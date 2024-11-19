564efc42363d9991908ba3f784a56655f966dc3a
kpilszak <klaudia.pilszak@gmail.com>
2024-11-19 06:17:24 -0500

    Add video player container

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,6 +1,17 @@
+import ReactPlayer from 'react-player'
+
 const Video = () => {
     return (
-        <div className="video-container"></div>
+        <div className="video-container">
+            <ReactPlayer 
+                url={"https://www.youtube.com/watch?v=PBrEq9Wd6_U"} 
+                muted={true}
+                controls={false}
+                playing={true}
+                width={'100%'}
+                height={'100%'}
+            />
+        </div>
     )
 }
 
