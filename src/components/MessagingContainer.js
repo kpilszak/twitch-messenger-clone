285133e32082ee72d7f2dd392d496f128ec7822d
kpilszak <klaudia.pilszak@gmail.com>
2024-11-19 05:57:04 -0500

    Style User List

--- src/components/MessagingContainer.js
+++ src/components/MessagingContainer.js
@@ -1,10 +1,12 @@
 import { ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react'
-import React from 'react'
+import { useState } from 'react'
 import { useCookies } from 'react-cookie'
 import UserList from './UserList'
+import { FaUsers, FaArrowAltCircleLeft } from 'react-icons/fa'
 
 const MessagingContainer = ({users}) => {
     const [cookies, setCookie, removeCookie] = useCookies(['user'])
+    const [userListVisible, setUserListVisible] = useState(false)
 
     const logout = () => {
         removeCookie('Name', cookies.Name)
@@ -17,13 +19,21 @@ const MessagingContainer = ({users}) => {
 
     return (
         <div className='messaging-container'>
-            <Window>
-                <ChannelHeader />
-                <MessageList />
-                <MessageInput />
-                <button className="standard-button" onClick={logout}>Logout</button>
-                <UserList users={users}/>
-            </Window>
+            {!userListVisible && (
+                <Window>
+                    <ChannelHeader />
+                    <MessageList />
+                    <MessageInput />
+                    <button className="standard-button" onClick={logout}>Logout</button>
+                    <UserList users={users} />
+                </Window>
+            )}
+            {userListVisible && (
+                <Window>
+                    <ChannelHeader title='Users' />
+                    <UserList users={users} />
+                </Window>
+            )}
             <Thread />
         </div>
     )
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,6 @@
-import { FaHorseHeas, FaSmile } from 'react-icons/fa'
+import { FaHorseHead, FaSmile } from 'react-icons/fa'
 
 const UserList = ({users}) => {
-    console.log(users);
     return (<div className="userlist-container">
         {users?.map(user => (<li key={user.id}>
             {user.role == 'user' ? <FaHorseHead/> : <FaSmile/>}
